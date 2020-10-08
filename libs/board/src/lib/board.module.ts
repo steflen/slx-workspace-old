73ca8efae77b92523b4ae9b063a1938aa6920959
stelen <stefan.lenz@barco.com>
2020-10-08 15:53:50 +0200

    minor

--- libs/board/src/lib/board.module.ts
+++ libs/board/src/lib/board.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
 import { SharedModule } from '@slx/shared';
+import { SharedMaterialModule } from '@slx/shared-material';
 import { BoardRoutingModule } from './board-routing.module';
 import { BoardComponent } from './components/board/board.component';
 import { ControlsComponent } from './components/controls/controls.component';
@@ -8,8 +10,12 @@ import { DynamicGridComponent } from './components/dynamic-grid/dynamic-grid.com
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, BoardRoutingModule],
+  imports: [CommonModule, SharedModule, SharedMaterialModule, BoardRoutingModule],
   declarations: [DynamicGridComponent, BoardComponent, ControlsComponent, TopBarComponent],
   entryComponents: [ControlsComponent],
+  providers: [
+    { provide: MatBottomSheetRef, useValue: {} },
+    { provide: MAT_BOTTOM_SHEET_DATA, useValue: {} },
+  ],
 })
 export class BoardModule {}
